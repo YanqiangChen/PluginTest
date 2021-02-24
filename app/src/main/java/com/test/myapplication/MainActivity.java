f282cb6bffc925388fa34a01715120864a3eddc9
yanqiangchen@ctrip.com <yanqiangchen@ctrip.com>
2021-02-24 22:48:49 +0800

    自动化埋点

--- app/src/main/java/com/test/myapplication/MainActivity.java
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -3,12 +3,22 @@ package com.test.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btnTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTest=findViewById(R.id.btn_test);
+        btnTest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
